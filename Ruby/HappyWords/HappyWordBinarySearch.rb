449f9753aa1c6d6307d542172a13903801a96de1
piekstra <piekstra17@up.edu>
2018-06-09 18:26:11 -0700

    Modifying Happy Word Binary Search implementation to use the HappyWordSearch class

--- Ruby/HappyWords/HappyWordBinarySearch.rb
+++ Ruby/HappyWords/HappyWordBinarySearch.rb
@@ -2,43 +2,45 @@ require_relative 'JoyWordWithOcurrences'
 require_relative 'SentenceWithJoyScore'
 require_relative 'SentenceGroupWithJoyScore'
 require_relative 'BinarySearch'
+require_relative 'HappyWordSearch'
 
-startTime = Time.now
+def Search(article, joyWords)
+    textSentences = article.scan(/[^\.!?]+[\.!?]/)
 
-joyWordFile = 'sample_joy_words.txt'
-textToSearch = 'sample_article_text.txt'
+    topSentence = SentenceWithJoyScore.new(nil)
+    currentSentenceGroup = SentenceGroupWithJoyScore.new
+    topSentenceGroup = SentenceGroupWithJoyScore.new
+    textTotalScore = 0
 
-joyWords = File.read(joyWordFile).downcase.split(', ').sort
-text = File.read(textToSearch)    
-textSentences = text.scan(/[^\.!?]+[\.!?]/)
-
-topSentence = SentenceWithJoyScore.new(nil)
-currentSentenceGroup = SentenceGroupWithJoyScore.new
-topSentenceGroup = SentenceGroupWithJoyScore.new
-# Total score for the entire text
-textTotalScore = 0
-
-textSentences.each do |sentence|
-    # Look for the Joy Words in the sentence
-    # The count of joy words is the score
-    sentenceWithJoyScore = SentenceWithJoyScore.new(sentence)
-    wordsToSearch = sentence.split(' ').map(&:downcase)
-    wordsToSearch.each do |word|
-        matchCount = BinarySearch.CountOccurrences joyWords, word
-        sentenceWithJoyScore.addJoyWordWithOcurrence(word, matchCount) if matchCount > 0
+    textSentences.each do |sentence|
+        # Look for the Joy Words in the sentence
+        # The count of joy words is the score
+        sentenceWithJoyScore = SentenceWithJoyScore.new(sentence)
+        wordsToSearch = sentence.split(' ').map(&:downcase)
+        wordsToSearch.each do |word|
+            matchCount = BinarySearch.CountOccurrences joyWords, word
+            sentenceWithJoyScore.addJoyWordWithOcurrence(word, matchCount) if matchCount > 0
+        end
+        
+        topSentence = sentenceWithJoyScore.dup if sentenceWithJoyScore.JoyScore > topSentence.JoyScore
+        
+        currentSentenceGroup.addSentence(sentenceWithJoyScore)   
+        topSentenceGroup = currentSentenceGroup.dup if currentSentenceGroup.JoyScore > topSentenceGroup.JoyScore
+        
+        textTotalScore += sentenceWithJoyScore.JoyScore
     end
-    
-    topSentence = sentenceWithJoyScore.dup if sentenceWithJoyScore.JoyScore > topSentence.JoyScore
-    
-    currentSentenceGroup.addSentence(sentenceWithJoyScore)   
-    topSentenceGroup = currentSentenceGroup.dup if currentSentenceGroup.JoyScore > topSentenceGroup.JoyScore
-    
-    textTotalScore += sentenceWithJoyScore.JoyScore
+
+    resultMessage = "Total article score: #{textTotalScore}\n"
+    resultMessage += "Highest single-sentence score: #{topSentence.JoyScore}\n"
+    resultMessage += "Highest sentence group score: #{topSentenceGroup.JoyScore}\n"
+
+    return resultMessage
 end
 
-endTime = Time.now
+article = HappyWordSearch.ReadArticle()
+words = HappyWordSearch.ReadSearchWords(sort: true)
+runtime, results = HappyWordSearch.TimedSearch(article, words, method(:Search))
 
-puts "Total score: #{textTotalScore}"
-puts "Highest scoring (#{topSentence.JoyScore}) sentence: #{topSentence.Sentence}"
-puts "Highest scoring (#{topSentenceGroup.JoyScore}) group of sentences: #{topSentenceGroup.Sentences}"
-puts "Time elapsed: #{endTime - startTime}"
+puts "Results: #{results}"
+puts "Elapsed time: #{runtime}"
+
