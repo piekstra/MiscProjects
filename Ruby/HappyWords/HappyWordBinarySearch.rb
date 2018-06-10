ccf60c6bf379bf96893d954594640ab9bb45a534
piekstra <piekstra17@up.edu>
2018-06-09 20:19:33 -0700

    Refactoring to reduce repeated code. This may have to change again soon when working out other search implementations

--- Ruby/HappyWords/HappyWordBinarySearch.rb
+++ Ruby/HappyWords/HappyWordBinarySearch.rb
@@ -1,45 +1,12 @@
-require_relative 'SentenceWithJoyScore'
-require_relative 'SentenceGroupWithJoyScore'
-require_relative 'BinarySearch'
 require_relative 'HappyWordSearch'
+require_relative 'BinarySearch'
 
-def Search(article, joyWords)
-    textSentences = article.scan(/[^\.!?]+[\.!?]/)
-
-    topSentence = SentenceWithJoyScore.new(nil)
-    currentSentenceGroup = SentenceGroupWithJoyScore.new
-    topSentenceGroup = SentenceGroupWithJoyScore.new
-    textTotalScore = 0
-
-    textSentences.each do |sentence|
-        # Look for the Joy Words in the sentence
-        # The count of joy words is the score
-        sentenceWithJoyScore = SentenceWithJoyScore.new(sentence)
-        wordsToSearch = sentence.split(' ').map(&:downcase)
-        wordsToSearch.each do |word|
-            found = BinarySearch.InArray joyWords, word
-            sentenceWithJoyScore.addJoyWord word if found
-        end
-        
-        topSentence = sentenceWithJoyScore.dup if sentenceWithJoyScore.JoyScore > topSentence.JoyScore
-        
-        currentSentenceGroup.addSentence(sentenceWithJoyScore)   
-        topSentenceGroup = currentSentenceGroup.dup if currentSentenceGroup.JoyScore > topSentenceGroup.JoyScore
-        
-        textTotalScore += sentenceWithJoyScore.JoyScore
-    end
-
-    resultMessage = "Total article score: #{textTotalScore}\n"
-    resultMessage += "Highest single-sentence score: #{topSentence.JoyScore}\n"
-    resultMessage += "Highest sentence group score: #{topSentenceGroup.JoyScore}\n"
-
-    return resultMessage
+def Find(joyWords, word)
+    return BinarySearch.InArray joyWords, word
 end
 
-article = HappyWordSearch.ReadArticle()
-words = HappyWordSearch.ReadSearchWords(sort: true)
-runtime, results = HappyWordSearch.TimedSearch(article, words, method(:Search))
+article = HappyWordSearch.ReadArticle
+words = HappyWordSearch.ReadSearchWords sort: true
 
-puts "Results: #{results}"
-puts "Elapsed time: #{runtime}"
+HappyWordSearch.TimedSearch article, words, method(:Find)
 
--- Ruby/HappyWords/HappyWordSearch.rb
+++ Ruby/HappyWords/HappyWordSearch.rb
@@ -1,3 +1,6 @@
+require_relative 'SentenceWithJoyScore'
+require_relative 'SentenceGroupWithJoyScore'
+
 class HappyWordSearch
     def self.ReadArticle(file='sample_article_text.txt')
         return File.read(file)
@@ -16,11 +19,41 @@ class HappyWordSearch
         end
     end
 
-    def self.TimedSearch(article, words, search)
+    def self.TimedSearch(article, joyWords, findMethod)
         startTime = Time.now
-        result = search.call(article, words)
+ 
+        textSentences = article.scan(/[^\.!?]+[\.!?]/)
+
+        topSentence = SentenceWithJoyScore.new(nil)
+        currentSentenceGroup = SentenceGroupWithJoyScore.new
+        topSentenceGroup = SentenceGroupWithJoyScore.new
+        textTotalScore = 0
+
+        textSentences.each do |sentence|
+            # Look for the Joy Words in the sentence
+            # The count of joy words is the score
+            sentenceWithJoyScore = SentenceWithJoyScore.new(sentence)
+            wordsToSearch = sentence.split(' ').map(&:downcase)
+            wordsToSearch.each do |word|
+                found = findMethod.call joyWords, word
+                sentenceWithJoyScore.addJoyWord word if found
+            end
+            
+            topSentence = sentenceWithJoyScore.dup if sentenceWithJoyScore.JoyScore > topSentence.JoyScore
+            
+            currentSentenceGroup.addSentence(sentenceWithJoyScore)   
+            topSentenceGroup = currentSentenceGroup.dup if currentSentenceGroup.JoyScore > topSentenceGroup.JoyScore
+            
+            textTotalScore += sentenceWithJoyScore.JoyScore
+        end
+
         endTime = Time.now
-        return endTime - startTime, result
+        runtime = endTime - startTime
+
+        puts "Total article score: #{textTotalScore}"
+        puts "Highest single-sentence score: #{topSentence.JoyScore}"
+        puts "Highest sentence group score: #{topSentenceGroup.JoyScore}"
+        puts "Elapsed time: #{runtime}"
     end
 end
 
