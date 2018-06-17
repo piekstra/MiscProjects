6309644f25bcf0e9e200f2a54c3261d776f22593
Caleb Piekstra <calebpiekstra@gmail.com>
2018-06-17 00:11:16 +0000

    Added optional console logging and headless running mode. These are specified via command line parameters. Defaults are no console logging output, and non-headless running mode.

--- Ruby/AutoRally/AutomatedRallyCheckInTabs.rb
+++ Ruby/AutoRally/AutomatedRallyCheckInTabs.rb
@@ -5,64 +5,80 @@ require 'watir-scroll'
 # Returns: email, password
 def validateInputParameters(args)   
     if ARGV.length < 2
-        puts "Expected username and password."
+        puts "Expected at least username and password."
         puts "User: #{ARGV[0]}"
         puts "Password: #{ARGV[1]}"
         exit
-    elsif ARGV.length > 2
-        puts "Too many arguments. Just enter username and password."
+    elsif ARGV.length > 4 
+        puts "Too many arguments. Just enter username and password or optionall specify console logging and headless operation."
         exit
     end
         
     # we expect a username and pass here
     email = ARGV[0]
     password = ARGV[1]
-    return email, password
+    consoleLogging = ARGV[2] ? (true if ARGV[2] == "true") : false
+    runHeadless = ARGV[3] ? (true if ARGV[3] == "true") : false
+    return email, password, consoleLogging, runHeadless
 end
 
-def login(browser, email, password)
+def login(browser, email, password, consoleLogging = false)
     # Login
+    
+    puts "Logging in" if consoleLogging
     browser.goto 'https://accounts.werally.com/'
     browser.text_field(id: 'loginEmail').set(email)
     browser.text_field(id: 'loginPassword').set(password)
     browser.button(type: 'submit').click
+    puts "Log in attempted" if consoleLogging
 end
 
-def goToMissionsPage(browser)
+def goToMissionsPage(browser, consoleLogging = false)
+    puts "Going to missions page" if consoleLogging
     # Goto Missions
     browser.elements(id: 'navMissionsBtn')[0].click
     # Wait for the page to finish loading what we need
     # We need the following element (the parent of the podContainer) to exist 
     # for the script we execute to get the totalMissions
     until browser.element(css: "div[ng-show='galleryMissions.loaded']").exists? do sleep 1 end
-    lazyLoadHandler(browser)
+    puts "Missions page loaded" if consoleLogging
+    lazyLoadHandler(browser, consoleLogging)
 end
 
-def lazyLoadHandler(browser)    
+def lazyLoadHandler(browser, consoleLogging = false)   
+    puts "Handling lazy loading" if consoleLogging	
     hardLimit = 1000
     #hardLimit = browser.execute_script("return angular.element(document.getElementsByClassName('podContainer')[0]).scope().$parent.limitTotal;")
     totalMissions = browser.execute_script("return angular.element(document.getElementsByClassName('podContainer')[0]).scope().$parent.galleryMissions.myMissions.length;")
     
     # Keep scrolling down through lazy loading - be wary of the hard limit
+    puts "Expecting #{totalMissions} elements" if consoleLogging
     loop do
         currentCount = browser.divs(class: 'missionsPod').size
+	puts "Currently loaded #{currentCount} elements" if consoleLogging
         break if currentCount == totalMissions || currentCount >= hardLimit
         browser.scroll.to :bottom
     end
+    puts "Fully loaded lazy page" if consoleLogging
 end
 
-def checkInMissions(browser)
+def checkInMissions(browser, consoleLogging = false)
+    puts "Checking in missions" if consoleLogging
     # Check In to all missions
     browser.divs(class: 'missionsPod').each do |pod|
     
         # Skip missions that can only be checked in on a mobile device
         mobileOnly = pod.div(class: 'mobileCheckIn').exists?
+	puts "Skipping mobile-only mission check in" if mobileOnly and consoleLogging
         next if mobileOnly
             
         progressTotal = pod.div(class: 'progressTotal').text   
         
         # Make sure the button is in view before we do anything with it
         pod.button.scroll.to:center
+	missionTitle = pod.h3(class: 'podHeader').text
+	missionId = pod.id
+	puts "Checking in mission (#{missionId}): #{missionTitle}" if consoleLogging
         
         # This Check In doesn't involve any progress numbers
         if progressTotal == ""
@@ -102,15 +118,15 @@ def booleanCheckIn(browser, pod)
 end
 
 def main() 
-    email, password = validateInputParameters(ARGV)
+    email, password, consoleLogging, runHeadless = validateInputParameters(ARGV)
     
     # Using Chrome for this  
-    browser = Watir::Browser.new :chrome
+    browser = Watir::Browser.new :chrome, headless: runHeadless
     
-    login(browser, email, password)    
-    goToMissionsPage(browser)
-    checkInMissions(browser)
-
+    login(browser, email, password, consoleLogging)    
+    goToMissionsPage(browser, consoleLogging)
+    checkInMissions(browser, consoleLogging)
+    puts "Finished" if consoleLogging
     browser.quit
 end
 
