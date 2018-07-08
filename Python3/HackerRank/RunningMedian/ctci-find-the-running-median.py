d27531f9e9d38870bffab57c8b10226018287803
Caleb Piekstra <piekstra17@up.edu>
2018-07-08 00:32:23 -0700

    Adding Running Median solution
    This implementation maintains a sorted array of numbers so the median is always at the middle of the array. To keep the array sorted, when new elements are added the code searches for an insertion point using a binary search.
    
    Because a binary search is used for insertion and median lookups are constant time, we have a runtime complexity of O(N lg N) to maintain the array's sorted order and O(N) for the median lookups. So the actual runtime complexity is O(N lg N) + O(N) or just O(N lg N).

---
+++ Python3/HackerRank/RunningMedian/ctci-find-the-running-median.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def insertIntoSortedList(list, val):    
+    start = 0
+    end = len(list)
+    while start < end:
+        mid = int((end - start)/2) + start
+        if list[mid] > val:
+            end = mid - 1
+        elif list[mid] < val:
+            start = mid + 1
+        else:
+            list.insert(mid, val)
+            return
+    
+    if len(list) == 0 or start == len(list) or list[start] > val:
+        list.insert(start, val)
+    else:
+        list.insert(start+1, val)
+        
+def getMedian(list):
+    mid = int(len(sortedList)/2)
+    if len(list) % 2 == 1:
+        return list[mid]
+    return (list[mid-1] + list[mid]) / 2
+
+if __name__ == '__main__':
+    n = int(input())
+    sortedList = []
+    
+    for _ in range(n):
+        a_item = int(input())
+        insertIntoSortedList(sortedList, a_item)
+        print ("%.1f" % getMedian(sortedList))
+        
+        
