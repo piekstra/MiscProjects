08e203630d3f1f7834dbd1e69ab95158076aeecb
piekstra <piekstra@users.noreply.github.com>
2021-03-15 18:01:18 -0700

    Rust guessing game

---
+++ Rust/guessing_game/README.md
@@ -0,0 +1,9 @@
+# Guessing Game
+
+This project was generated using the following command:
+
+```
+cargo new guessing_game
+```
+
+The code was written following this tutorial: https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
---
+++ Rust/guessing_game/src/main.rs
@@ -0,0 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        let result = read_and_set_guess(&mut guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", result);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+fn read_and_set_guess(guess: &mut String) -> String {
+    io::stdin().read_line(guess).expect("Failed to read line");
+
+    guess.clone()
+}
